7fe392c962f53f65723107f44bcd6bf25c71d566
aesperer <piyrw9754@gmail.com>
2024-04-15 10:51:22 +0900

    add stack, segement asm

--- src/main.asm
+++ src/main.asm
@@ -1,11 +1,23 @@
 org 0x7C00
 bits 16
 
-main:
-    hit
 
+
+
+
+main:
+    : setup data segments
+    mov ax, 0   : can't wirte to ds/es directly
+    mov ds, ax
+    mov es, ax
+
+    : setup stack   : stack grows downwards from where we are loaded in memory
+    mov ss, ax
+    mov sp, 0x7C00
+
+    hit
+    
 .halt:
     jmp .halt
-
 times 510-($0-$$) db 0
 dw 0AA55h
